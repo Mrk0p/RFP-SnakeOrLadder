25bd8f47de02dde16c8564cd2526ada3e445f96e
Mrk0p <sachinwele23@gmail.com>
2023-01-18 17:55:06 +0530

    Added : Starting position logic

---
+++ src/com/bridgelabz/snake_or_ladder/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.snake_or_ladder;
+
+public class SnakeAndLadder {
+
+    public static void main(String[] args) {
+        int position = 0;
+        System.out.println("player at start:"+position);
+    }
+}
